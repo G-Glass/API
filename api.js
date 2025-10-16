e2f9cc785a9dacefae4759058b6aa5d7ae6f7ddb
Sataca <glassg767@gmail.com>
2025-10-16 20:00:04 +0200

    committed

--- api.js
+++ api.js
@@ -1,11 +1,29 @@
 //API
-
 import express from "express";
-const app = express();
-const PORT = 3000;
+import cors from 'cors';
 
+const app = express();
+const PORT = 3008;
+
+const arreyresponse = [
+    {"filme":"O Mundo do ser","Ano":2025}
+]
+
+const dados = [
+    {
+        "Exemplo":"Este é outro exemplo JSONde ",
+        "Passos":22,
+        "Carro":"Mazda",
+        "Modelo":"Subprime",
+        "Especificações":[
+            {"Luz":"verde"},
+            {"Preço":144.234},
+            {"Ano":2020}
+        ]
+    }
+]
 app.get("/", (req, res) => {
-    res.send("Olá, Mundo");
+    res.json(dados);
 });
 
 app.listen(PORT, () => {
