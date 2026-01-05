1faf8872e2422c4938d57d2bc00d302ec446b8bc
Sataca <glassg767@gmail.com>
2026-01-05 20:39:08 +0200

    commit

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log(`hi`)
+import express from "express";
+import recurso from "./server.json" with {type : "json"};
+
+const app = express();
+const PORT = 3004;
+
+app.get('/', (req,res) => {res.json(recurso)});
+
+app.listen(PORT, () => console.log(`Rotando no servidor ${PORT}`));
