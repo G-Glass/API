e0281b456c71bf70b6263ef042264caaa611f8e6
Sataca <glassg767@gmail.com>
2026-01-11 10:59:23 +0200

    commit

--- CRUD/SignUp.js
+++ CRUD/SignUp.js
@@ -4,7 +4,7 @@ const signUpSchema = new mongoose.Schema({
     nome: String,
     email: String,
     senha: mongoose.Schema.Types.Mixed,
-    novasenha: mongoose.Schema.Types.Mixed
+    conf_senha: mongoose.Schema.Types.Mixed
 });
 
 export default mongoose.model('signUp', signUpSchema)
